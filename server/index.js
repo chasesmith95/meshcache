e34f90d0ab6ce375ae75b82b5a06f9476b36fc36
Richard <yuukifushimi@gmail.com>
2019-04-25 16:03:23 -0400

    Registry API Calls

--- server/index.js
+++ server/index.js
@@ -26,10 +26,31 @@ server.applyMiddleware({ app, path });
 app.use("/graphiql", graphql({ schema, graphiql: true }));
 app.use("/api", sofa({ schema }));
 
+app.get('/api/getContractAddress', (req, res) => {
+  res.json({ address: Registry.getContractAddress() });
+})
+
+app.get('/api/getServices', (req, res) => {
+  res.json({ services: Registry.getServices() });
+})
+
+app.get('/api/getService/:serviceId', function (req, res) {
+  const serviceId=parseInt(req.params["serviceId"]);
+  res.json({ serviceId: serviceId, service: Registry.getService(serviceId) });
+})
+
+app.get('/api/getBootstraps/:serviceId', function (req, res) {
+  const serviceId=parseInt(req.params["serviceId"]);
+  res.json({ serviceId: serviceId, service: Registry.getBootstraps(serviceId) });
+})
+
+
 app.listen({ port: 4000 }, () =>
 {
   console.log(`🚀 GraphQL Server ready at http://localhost:4000${server.graphqlPath}`);
-  console.log(`🚀 REST API Server ready at http://localhost:4000/api/ (e.g. http://localhost:4000/api/dapps/)`);
+  console.log(`🚀 REST API Server ready at http://localhost:4000/api/ 
+                    (e.g. http://localhost:4000/api/dapps/
+                          http://localhost:4000/api/getContractAddress/)`);
   console.log(`🚀 GraphiQL Server ready at http://localhost:4000/graphiql/ `);
 }
 );
