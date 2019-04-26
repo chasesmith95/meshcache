fa9a2e3a1602794e28c90586bdd55025f788d17e
Richard <yuukifushimi@gmail.com>
2019-04-26 13:43:37 -0400

    explorer

--- meshcache-explorer/src/layouts/Server.js
+++ meshcache-explorer/src/layouts/Server.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 import * as web3Utils from "../util/web3Utils";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import CloudIcon from '@material-ui/icons/Cloud';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: '100%',
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+  card: {
+    minWidth: 275,
+  },
+});
 
 class Server extends Component {
   constructor(props) {
@@ -11,7 +37,7 @@ class Server extends Component {
     this.state={
       service : "",
       table : [],
-      node : "node card",
+      node : [],
       subscribers : "subscribers card",
       name : "{service registry}",
       contractAddress : ''
@@ -22,6 +48,8 @@ class Server extends Component {
 
   componentDidMount() {
     this.getEthData();
+    document.title = "Server Registry";
+  
   }
 
   getEthData=() => {
@@ -41,47 +69,117 @@ class Server extends Component {
 
   getService=(serviceId) => {
     var self=this;
+    self.setState({name: 'Server ' + serviceId})
     web3Utils.getService(serviceId).then(service => {
-      self.setState({service:JSON.stringify(service)});
+      console.log(service);
+      self.setState({ service: service[0].toString(), 
+                      name: service[1], 
+                      id: service[2], 
+                      owner: service[3], 
+                      stake: service[4].toString()});
+    }).catch(error => {
+      self.setState({ service: '',
+        name: '',
+        id: serviceId,
+        owner: '',
+        stake: ''
+  
+      });
     });
     web3Utils.getBootstraps(serviceId).then(bootstraps => {
-      self.setState({node:bootstraps.toString()});
+      var seen={};
+      bootstraps.map(bootstrap => {
+        seen[bootstrap]=1;
+        return bootstrap;
+      })
+   
+      self.setState({node:Object.keys(seen)});
+    }).catch(error => {
+      self.setState({node:[]});
     });
   }
  
   render() {
+    const { classes } = this.props;
     var self=this;
+
     return (
      
       <div className="App">
-        <header>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1> Name: {this.state.name} </h1>
-          <h3> Contract Address: {this.state.contractAddress} </h3>
-        </header>
+            <GridList className={classes.gridList}>
+                        
+              {this.state.table.map(serviceId => {
+                return <GridListTile key={"Server" + serviceId} 
+                          onClick={() => {
+                            self.getService(serviceId);
+                        }}
+                          
+                          >
+                  <GridListTileBar
+                    title={"Server " + serviceId}
+                    actionIcon={
+                      <IconButton className={classes.icon}>
+                        <CloudIcon />
+                      </IconButton>
+                    }
+                    onMouseEnter={() => {
+                      document.body.style.cursor='pointer';
+                    }}
+                    onMouseLeave={() => {
+                      document.body.style.cursor='initial';
+                      
+                    }}
+                  />
+                </GridListTile>
+              })}
+            </GridList>
 
 
-        {this.state.service ? 
-        <div className = "App-stats">
-          <h1>Service: {this.state.service}</h1>
-          <h1>Node: {this.state.node}</h1>
-          <h1>Subscribers: {this.state.subscribers}</h1>
+        {this.state.service && this.state.node ? 
+        <Card className={classes.card}>
+        <CardContent>
+          <div className = "App-stats">
+          <table width={'100%'}>
+            <tbody>
+                <tr>
+                     <td style={{width: '50%', textAlign:'right'}}><b>Server:</b></td>
+                     <td style={{width: '50%', textAlign:'left'}}> {this.state.service}
+                     </td>
+                 </tr>
+                 <tr>
+                     <td style={{width: '50%', textAlign:'right'}}><b>Name:</b></td>
+                     <td style={{width: '50%', textAlign:'left'}}>{this.state.name}</td>
+                 </tr>
+                 <tr><td style={{width: '50%', textAlign:'right'}}><b>Contract Address:</b></td>
+                     <td style={{width: '50%', textAlign:'left'}}> {this.state.contractAddress}</td>
+                 </tr>
+                 <tr><td style={{width: '50%', textAlign:'right'}}><b>Owner:</b></td>
+                     <td style={{width: '50%', textAlign:'left'}}>{this.state.owner}</td>
+                 </tr>
+                 <tr><td style={{width: '50%', textAlign:'right'}}><b>ID:</b> </td>
+                     <td style={{width: '50%', textAlign:'left'}}>{this.state.id}</td>
+                 </tr>
+                 <tr><td style={{width: '50%', textAlign:'right'}}><b>Stake:</b></td>
+                     <td style={{width: '50%', textAlign:'left'}}>{this.state.stake}</td>
+                 </tr>
+                 <tr><td style={{width: '50%', textAlign:'right'}}><b>Node:</b></td>
+                     <td style={{width: '50%', textAlign:'left'}}>
+                          {this.state.node.map(address => {
+                        return <div key={address}><font size={'2'}>
+                        <a href='http://localhost:4000/graphql' target="_newWindow">
+                        {address}</a></font></div>
+                      })}</td>
+                 </tr>
+                 <tr><td style={{width: '50%', textAlign:'right'}}><b>Subscribers:</b></td>
+                     <td style={{width: '50%', textAlign:'left'}}></td>
+                 </tr>
+            </tbody>
+          </table>
+          
         </div>
+        </CardContent>
+        </Card>
         : null}
-
-
-      <div className = "App-table">
-        <h1>Services: {this.state.table.map(serviceId => {
-            return <span>
-              [
-              <a href='#table' onClick={() => {
-                self.getService(serviceId);
-            }}>{serviceId}</a>
-              ]
-            </span>
-        })}</h1>
-      </div>
-
 
       </div>
 
@@ -90,4 +188,8 @@ class Server extends Component {
   }
 }
 
-export default Server;
+Server.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Server);
